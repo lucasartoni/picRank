ccce8bf02168c6e5d0577924a93a36d53de887a0
Luca Sartoni <info@lucasartoni.com>
2013-12-30 18:22:36 +0100

    Flickr API call fixed and UI restyled

--- js/models.js
+++ js/models.js
@@ -14,7 +14,7 @@ var Picture = Backbone.Model.extend({
 var Pictures = Backbone.Collection.extend({
 	model: Picture,
 	fetchPics: function(){
-		var query = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8718c0ad9a45fd820340d220ec094b8b&user_id=21925196%40N08&max_upload_date=&min_taken_date=2013-12-01&max_taken_date=2013-12-31&format=json&nojsoncallback=1&auth_token=72157639104048186-d0f6221716b894e5&api_sig=a2e9b235bec1e995f127036b2d6adb34';
+		var query = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1f8fdc67dc07dc24dfabdf20feb9866e&user_id=21925196%40N08&format=json&nojsoncallback=1'
 		var that = this;
 		$.getJSON(query).done(function(response){
 			for (var i=0; i<response.photos.photo.length;i++){
